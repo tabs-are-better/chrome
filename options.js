ee948328b22dc00b3dc55809c609bcac46ad06a7
Cedric van Putten <me@bycedric.com>
2015-10-09 19:46:17 +0200

    Updated code to tabs

--- options.js
+++ options.js
@@ -1,32 +1,32 @@
 function getInputElement() {
-    return document.getElementById('characters');
+	return document.getElementById('characters');
 }
 
 function setDoneTimeout() {
-    if (window._timer) {
-        window.clearTimeout(window._timer);
-    }
+	if (window._timer) {
+		window.clearTimeout(window._timer);
+	}
 
-    window._timer = window.setTimeout(function () {
-        getInputElement().className = 'o-input';
-    }, 1000);
+	window._timer = window.setTimeout(function () {
+		getInputElement().className = 'o-input';
+	}, 1000);
 }
 
 // fetch current value on load
 window.addEventListener('load', function () {
-    chrome.storage.sync.get('characters', function(settings) {
-        if (settings.characters) {
-            getInputElement().value = settings.characters;
-        }
-    });
+	chrome.storage.sync.get('characters', function(settings) {
+		if (settings.characters) {
+			getInputElement().value = settings.characters;
+		}
+	});
 });
 
 // listen for input changes
 getInputElement().addEventListener('input', function (event) {
-    event.target.className = 'o-input o-input--loading';
+	event.target.className = 'o-input o-input--loading';
 
-    chrome.storage.sync.set({ 'characters': event.target.value }, function() {
-        event.target.className = 'o-input o-input--done';
-        setDoneTimeout();
-    });
+	chrome.storage.sync.set({ 'characters': event.target.value }, function() {
+		event.target.className = 'o-input o-input--done';
+		setDoneTimeout();
+	});
 });
--- tab-sizer.js
+++ tab-sizer.js
@@ -1,33 +1,33 @@
 function injectTabSize(size) {
-    var css = ['-webkit-', '-moz-', '-ms-', '-o-', '', '']
-        .join('tab-size: ' + (size || 4) + ';');
+	var css = ['-webkit-', '-moz-', '-ms-', '-o-', '', '']
+		.join('tab-size: ' + (size || 4) + ';');
 
-    var style = document.createElement('style');
-        style.type = 'text/css';
-        style.className = 'chose-own-tab-size';
-        style.appendChild(document.createTextNode('* {' + css + '}'));
+	var style = document.createElement('style');
+		style.type = 'text/css';
+		style.className = 'chose-own-tab-size';
+		style.appendChild(document.createTextNode('* {' + css + '}'));
 
-    document.head.appendChild(style);
+	document.head.appendChild(style);
 }
 
 function clearInjections() {
-    var injections = document.getElementsByClassName('chose-own-tab-size');
+	var injections = document.getElementsByClassName('chose-own-tab-size');
 
-    for (var node in injections) {
-        if (injections[node].parentNode) {
-            injections[node].parentNode.removeChild(injections[node]);
-        }
-    }
+	for (var node in injections) {
+		if (injections[node].parentNode) {
+			injections[node].parentNode.removeChild(injections[node]);
+		}
+	}
 }
 
 // listen to live changes
 chrome.storage.onChanged.addListener(function (settings) {
-    clearInjections();
-    injectTabSize(settings.characters.newValue);
+	clearInjections();
+	injectTabSize(settings.characters.newValue);
 });
 
 // let's fetch the current value
 chrome.storage.sync.get('characters', function(settings) {
-    clearInjections();
-    injectTabSize(settings.characters);
+	clearInjections();
+	injectTabSize(settings.characters);
 });
